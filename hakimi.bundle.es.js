b3730b46188faa5140ad1a7baca7e580229bf957
Muhammad Yasir Maulana <32666337+yasirmaulana@users.noreply.github.com>
2025-06-24 09:54:12 +0700

    Update hakimi.bundle.es.js

--- hakimi.bundle.es.js
+++ hakimi.bundle.es.js
@@ -6364,7 +6364,7 @@ const Jh = { class: "chat-get-started" }, Yh = /* @__PURE__ */ Ve({
       })
     ]));
   }
-}), Xh = {}, Qh = { class: "chat-powered-by" }, ep = /* @__PURE__ */ he("a", { href: "https://n8n.io?utm_source=n8n-external&utm_medium=widget-powered-by" }, "n8n", -1);
+}), Xh = {}, Qh = { class: "chat-powered-by" }, ep = /* @__PURE__ */ he("a", { href: "https://www.binwaris.com/" }, "BWN Tech", -1);
 function tp(t, e) {
   return Z(), ce("div", Qh, [
     Zs(" Powered by "),
